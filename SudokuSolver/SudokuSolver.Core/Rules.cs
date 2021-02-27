ee20521f3fc10ef2eda2e22749c992c5de2458d2
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2021-02-27 07:55:27 -0500

    Fixed issue with board indexes

--- SudokuSolver/SudokuSolver.Core/Rules.cs
+++ SudokuSolver/SudokuSolver.Core/Rules.cs
@@ -4,39 +4,56 @@ namespace SudokuSolver.Core
 {
     public class Rules
     {
-        public (int, SquareGroup) InnerSquareEliminationRule(SquareGroup group)
+        public RuleResult InnerSquareEliminationRule(SquareGroup group)
         {
             int squaresSolved = 0;
+            int i = 0;
 
             //First mark all of the possible numbers in available squares, within the square group
             foreach (int item in group.SolvedSquares)
             {
-                for (int y = 0; y <= 3; y++)
+                i = 0;
+                for (int y = 0; y < 3; y++)
                 {
-                    for (int x = 0; x <= 3; x++)
+                    for (int x = 0; x < 3; x++)
                     {
-                        if (group.Squares[y + x].CurrentSquare == 0)
+                        if (group.Squares[i].CurrentSquare == 0)
                         {
-                            group.Squares[y + x].EliminatePossibleSquare(item);
+                            group.Squares[i].EliminatePossibleSquare(item);
                         }
+                        i++;
                     }
                 }
             }
 
             //Then run a simple elimination, to see if the item can be solved
-            for (int y = 0; y <= 3; y++)
+            i = 0;
+            for (int y = 0; y < 3; y++)
             {
-                for (int x = 0; x <= 3; x++)
+                for (int x = 0; x < 3; x++)
                 {
-                    if (group.Squares[y + x].CurrentSquare == 0 && group.Squares[y + x].PossibleSquareCount == 1)
+                    if (group.Squares[i].CurrentSquare == 0 && group.Squares[i].PossibleSquareCount == 1)
                     {
                         squaresSolved++;
-                        group.Squares[y + x].CurrentSquare = group.Squares[y + x].PossibleSquaresFiltered[0];
+                        group.Squares[i].CurrentSquare = group.Squares[i].PossibleSquaresFiltered[0];
                     }
+                    i++;
                 }
             }
 
-            return new(squaresSolved, group);
+            return new RuleResult(squaresSolved, group);
+        }
+    }
+
+    public class RuleResult
+    {
+        public int SquaresSolved;
+        public SquareGroup Group;
+
+        public RuleResult(int squaresSolved, SquareGroup group)
+        {
+            SquaresSolved = squaresSolved;
+            Group = group;
         }
     }
 }
